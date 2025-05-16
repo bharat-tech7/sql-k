cac205990b9ed555d7cbf1f895becc1faadc00c2
Bharat Kumawat <132746357+bharat-tech7@users.noreply.github.com>
2025-05-16 13:17:58 +0530

    Add files via upload

--- Sql 1.sql
+++ Sql 1.sql
@@ -17,3 +17,27 @@ select * from actor where actor_id between 11 and 25;
 select * from actor where actor_id in(2,5);
 
 
+-- Distinct(unqiue value), count(count the rows)
+select distinct(first_name) from actor;
+select count(first_name) from actor;
+select count(distinct(first_name)) from actor;
+
+-- Functions 
+-- scaler function=> 
+-- Single function-- 1 row =>apply =>result for each row 
+-- multi-row functions  
+-- String Functions 
+select first_name, lower(first_name),upper(first_name),length(first_name),substr(first_name,1),substr(first_name,2,3),substr(first_name,-3), substr(first_name,-3,2) from actor;
+-- # substr(col,index,total_character)
+select first_name ,instr(first_name,'I') from actor;
+select first_name,last_name,concat(first_name,'-',last_name,'$')from actor;
+select first_name,last_name,concat_ws('-',first_name,last_name,'$')from actor;
+-- concat for add the string 
+-- concat_ws add the string with seperator 
+-- trim() remove wildspace from start and end 
+select first_name, trim('             h     hey             ')from actor;
+-- lpad(),rpad() 
+select first_name,rpad(first_name ,6,'$'), lpad(first_name,5,'#') from actor;
+
+
+
